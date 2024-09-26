ce3805071371ade12bc22abebf3540b7c22fdfc6
omosungodswill <omosun.godswill@gmail.com>
2024-09-27 00:18:37 +0100

    commit

---
+++ week1day2/assignment2.js
@@ -0,0 +1,4 @@
+// Use a for loop to print the numbers from 1 to 10.
+for (let x = 1; x <= 10; x++) {
+    console.log(x)
+}
---
+++ week1day2/assignment3.js
@@ -0,0 +1,8 @@
+//  Use a function to calculate and return the sum of two numbers, then print the result.
+let a = 5;
+let b = 17;
+function summation(a, b) {
+    return a + b;
+}
+let add = summation(a,b)
+console.log(add)
