13a3dacba1070dab8a17b9eb371ee6600ea8d7d3
omosungodswill <omosun.godswill@gmail.com>
2024-09-27 02:07:18 +0100

    2am assignments man

---
+++ week3day3/exercise2.js
@@ -0,0 +1,17 @@
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    reject(new Error("Data cannot be retrieved"))}, 5000); 
+        });
+};
+
+async function getData() { 
+    try {
+    const data = await fetchData();
+    console.log(data); 
+        } 
+    catch (error) {
+    console.log("Error:", error.message);
+    } 
+    getData();
+}
---
+++ week3day3/exercise3.js
@@ -0,0 +1,13 @@
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    reject(new Error("404 Page not found"))}, 5000)
+    });
+};
+
+fetchData().then((file) => {
+    console.log(file); 
+    })
+    .catch((error) => {
+    console.log("Error:", error.message); 
+});
