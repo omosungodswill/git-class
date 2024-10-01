9ede7cbd318fbd92ea09824c4a3acfea9003fadd
omosungodswill <omosun.godswill@gmail.com>
2024-10-01 18:20:03 +0100

    Tomi's server

---
+++ week4day1/express.js
@@ -0,0 +1,13 @@
+const express = require("express"); const app = express();
+
+app.get("/", (req, res) => { 
+    res.send("Hello, Express!");
+}); 
+
+app.get("/about", (req, res) => { 
+    res.send("About page")
+})
+
+app.listen(3000, () => {
+console.log("Express server running on port 3000");
+});
