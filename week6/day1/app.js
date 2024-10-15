8057f70499810ed2e0a652fd047e1748ea276851
omosungodswill <omosun.godswill@gmail.com>
2024-10-15 23:43:01 +0100

    restful apis day 1

--- week6/day1/app.js
+++ week6/day1/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 app.use(express.json());
 
 app.get('/', (req,res) => {
--- week6/day1/userRoutes.js
+++ week6/day1/userRoutes.js
@@ -4,3 +4,10 @@ const port = process.env.port || 3000
 app.listen(port, () => {
     console.log(`Server running on Port ${port}`)
 })
+
+app.get("/users", (req,res) => {
+    const users = [
+        {user_id: 1, name: "Godswill"},
+        {user_id: 2, name: "Charles"}];
+    res.send({users})
+})
